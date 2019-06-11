2bf5220ab0bf8a4265ba93e217117d6e36fddf8c
Jonathan Peralez <jperalez@gmail.com>
2019-06-11 12:17:56 -0700

    add styles to header and index. add font awesome. add normalize.css

--- src/Containers/HeaderContainer.js
+++ src/Containers/HeaderContainer.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React from 'react';
+import './HeaderContainer.css';
 
- const HeaderContainer = () => {
+const HeaderContainer = () => {
   return (
-    <div className="header"> 
-      Header Container 
+    <div className="fluid-container white">
+      <div className="fluid-inner">
+        <div className="inner-flex-desktop">
+          <h1>ReservationSmith</h1>
+          <div className="occupied-indicator">
+            <i class="fa fa-times-circle"></i><span> Room is occupied</span>
+          </div>
+        </div>
+        <div className="inner-flex-mobile">
+          <div>
+            <div>
+              <i class="fas fa-bars icon-margin-left"></i>
+              <h1 className="inline">RS</h1>
+            </div>
+          </div>
+          <i className="fa fa-times-circle"></i>
+        </div>
+      </div>
     </div>
-  )
-
-}
+  );
+};
 
 export default HeaderContainer;
--- src/Containers/SignupContainer.js
+++ src/Containers/SignupContainer.js
@@ -1,12 +1,12 @@
 import React from "react";
+import './SignupContainer.css';
 
 const SignupContainer = ()  => {
   return (
     <div className="container"> 
-      SignUp Container 
+      <h1>Welcome to Reservation Smith</h1>
     </div>
-  )
-
-}
+  );
+};
 
 export default SignupContainer;
