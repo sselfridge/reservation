e0d1c66e15189460566f824c44b58aa043056e56
Sam Selfridge <Sam.selfridge@gmail.com>
2019-06-12 15:19:34 -0700

    testing things on pi

--- src/Containers/SignupContainer.js
+++ src/Containers/SignupContainer.js
@@ -1,7 +1,20 @@
 import React from "react";
 import './SignupContainer.css';
 
+
+
+
+
 const SignupContainer = ()  => {
+ 
+  fetch('/events')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+  });
+
   return (
     <div className="container"> 
       <h1>Welcome to Reservation Smith</h1>
